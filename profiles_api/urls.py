cb94c747ee71b10092295f00c2cbedcb60223e24
mohammadreza mohammadi <mohammadrezamohammadi@mohammadrezas-MacBook-Air.local>
2020-02-22 14:22:25 +0330

    Added view set to profiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path ,include
+from rest_framework.routers import DefaultRouter
 from  profiles_api import views
-from django.urls import path, re_path
 
 
+
+router = DefaultRouter()
+router.register ('hello-viewset',views.HelloViewSet,basename = 'hello-viewset')
 
 urlpatterns = [
     path('hello-view/',views.HelloAPIView.as_view()), 
+    path('',include(router.urls))
 ]
